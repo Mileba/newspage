aa7d381246d5fdee4c740ceb9fb97246be2906b6
Mileba <33633920+Mileba@users.noreply.github.com>
2019-09-17 10:27:09 +0100

    Updated

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ const DEFAULT_HPP = '100';
 const PATH_BASE = 'https://hn.algolia.com/api/v1';
 const PATH_SEARCH = '/search';
 const PARAM_SEARCH = 'query=';
-const PARAM_PAGE = 'page';
-const PARAM_HPP = 'hitsPerPage';
+const PARAM_PAGE = 'page=';
+const PARAM_HPP = 'hitsPerPage=';
 
 const largeColumn = {
   width: '40%',
